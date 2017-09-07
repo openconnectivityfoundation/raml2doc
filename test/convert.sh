4c8ec27f17134aae72987ccbc7edea096dab368f
Wouter van der Beek <wovander@cisco.com>
2017-09-07 15:43:36 +0100

    fixes for swagger generation

--- test/convert.sh
+++ test/convert.sh
@@ -132,9 +132,10 @@ cp $IN_DIR/schemas/* $OUTPUT_DIR/copy-resolved/schemas/.
 
 for file in $IN_DIR$SCHEMA_DIR/*.json
 do
-    echo "converting $file to $OUTPUT_DIR/copy-resolved/schemas/$(basename $file)"
-    node node-resolver.js $file
-    cp temp.json $OUTPUT_DIR/copy-resolved$SCHEMA_DIR/$(basename $file)
+    if [[ $file != *".swagger.json" ]]; then
+        echo "converting $file to $OUTPUT_DIR/copy-resolved$SCHEMA_DIR/$(basename $file)"
+        node node-resolver.js $file    >  $OUTPUT_DIR/copy-resolved$SCHEMA_DIR/$(basename $file)
+    fi
 done
 
 IN_DIR=$OUTPUT_DIR/copy-resolved
--- test/node-resolver.js
+++ test/node-resolver.js
@@ -1,12 +1,11 @@
 
 //https://github.com/BigstickCarpet/json-schema-ref-parser/blob/master/docs/README.md
-var fs = require('fs');
 var $RefParser = require('json-schema-ref-parser');
+var resolveAllOf = require('json-schema-resolve-allof');
 
 //$RefParser.bundle(process.argv[2])
 $RefParser.dereference(process.argv[2])
   .then(function(schema) {
-    var output = '';
-    output = JSON.stringify(schema);
-    fs.writeFile("temp.json", output)
+    data = resolveAllOf(schema);
+    console.log(JSON.stringify(data));
   });
