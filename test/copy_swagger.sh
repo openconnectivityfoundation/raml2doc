31240de76acdfdbc9760672d9b6cbdd1a22c3b94
Wouter van der Beek <wovander@cisco.com>
2017-09-11 14:54:27 +0100

    copying generated swagger files to toplevel output directory

---
+++ test/copy_swagger.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# copying all generated swagger files to top level directory.
+#
+
+
+OUTPUT_DIR=../../out-oneIOTa-2/
+
+copy_from=$1
+#copy_from=$OUTPUT_DIR
+
+for mydir in $copy_from/*
+do
+    if [[ -d $mydir ]]; then
+        if [[ $mydir != "copy-resolved" ]]; then
+            for file in $mydir/*.swagger.json
+            do
+                echo "$file"
+                cp  "$file" $copy_from/.
+            
+            done
+        fi
+    fi 
+done
